b3292d1498f6ea076a5b1373caae4a1aa011b9a1
Daniel Roach <dannycr09@hotmail.com>
2018-02-16 11:54:10 -0500

    Adding some classes

---
+++ model/classes/beer.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ */
+class Beer
+{
+
+  private $uid, $fName, $lName, $email;
+
+  public function getId()
+  {
+    return
+  }
+
+  public function getFirstName()
+  {
+    return
+  }
+
+  public function getLastName()
+  {
+    return
+  }
+
+}
