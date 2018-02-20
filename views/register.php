5da871ff5fbd40ac12ac768647ce6710bf9be1df
Moisés Lora Pérez <mal3941@rit.edu>
2018-02-19 19:10:11 -0500

    Added Login and Register pages

--- views/register.php
+++ views/register.php
@@ -6,7 +6,7 @@
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <!-- <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
 
@@ -42,38 +42,41 @@
         <div class="card hoverable col s6 offset-s3">
           <form>
             <div class="card-content">
-              <span class="card-title">Log in</span>
+              <span class="card-title center-align">Register</span>
               <div class="row">
                 <div class="input-field col s12">
                   <label for="first_name">First Name</label>
-                  <input type="text" class="validate" name="first_name" id="first_name" />
+                  <input type="text" class="validate" name="first_name" id="first_name" >
+                </div>
+                <div class="input-field col s12">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" class="validate" name="last_name" id="last_name">
+                </div>
+                <div class="input-field col s12">
+                  <label for="email">Email address</label>
+                  <input type="email" class="validate" name="email" id="email" >
                 </div>
                 <div class="input-field col s12">
                   <label for="password">Password </label>
-                  <input type="password" class="validate" name="password" id="password" />
+                  <input type="password" class="validate" name="password" id="password">
                 </div>
               </div>
             </div>
             <div class="card-action center-align">
-              <input type="submit" class="btn brown waves-effect waves-light" value="Login">
+              <input type="submit" class="btn brown waves-effect waves-light" value="Register">
             </div>
-            <div class="card-action center-align">
-              <a href="views/register.php"> Don't have an account yet? Register now! </a>
-            </div>
-            <div class="card-action center-align">
-              <h5> Try out a new beer today!! </h5>
-              <input type="submit" class="btn brown waves-effect waves-light" value="Search">
-            </div>
-          </form>
+            </form>
+
         </div>
       </div>
     </div>
   </div>
 
-  <footer class="page-footer orange">
+  <footer class="page-footer brown">
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      Made by <a class="orange-text text-lighten-3" href="https://github.com/danielroach9/we-re-beer-for-you">We're beer for you</a>
+      - &copy; Copyright <?php echo date("Y"); ?>
       </div>
     </div>
   </footer>
