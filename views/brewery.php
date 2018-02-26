2b994f35fa1f4106c2518367382827b8602a000b
Daniel Roach <dannycr09@hotmail.com>
2018-02-26 11:42:58 -0500

    Fixed foreach?

--- views/brewery.php
+++ views/brewery.php
@@ -37,13 +37,13 @@ $brewery_beers = $db->getBeersByBrewery($id);
     <div class="col s9" id="beers-listing">
       <?php
       foreach ($brewery_beers as $value) {
-        echo "<div class="card small">
-          <div class="card-content white-text">
-          <span class="card-title">echo $value['name']</span>
-          <p>Category: echo "$value['cat_name']"</p>
-          <p>Style: echo "$value['style_name']"</p>
+        echo "<div class='card small'>
+          <div class='card-content white-text'>
+          <span class='card-title'>$value['name']</span>
+          <p>Category: $value['cat_name']</p>
+          <p>Style:  $value['style_name']</p>
           </div>
-        </div>"
+        </div>";
       }
       ?>
     </div>
