ec27ca6bdf74f7e3a104bb020911d6a274c250a3
Jack Corrigan <jcorrigan14@gmail.com>
2018-02-26 18:58:51 -0500

    fixed include error

--- views/preference_setup.php
+++ views/preference_setup.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <!-- <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
 
-<?php include "views/inc/header.php"; ?>
+<?php include "inc/header.php"; ?>
 <title>We're beer for you!</title>
 
 </head>
