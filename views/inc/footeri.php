ac3d3f1e6f9cb6bc1d30586885c80818cb358882
Anthony Perez <ajp8707@g.rit.edu>
2018-03-05 18:49:39 -0500

    footer

--- views/inc/footeri.php
+++ views/inc/footeri.php
@@ -9,7 +9,7 @@
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="../js/materialize.min.js"></script>
-  <script src="../js/scripts.js"></script>
+  <script src="./js/materialize.min.js"></script>
+  <script src="./js/scripts.js"></script>
   </body>
 </html>
