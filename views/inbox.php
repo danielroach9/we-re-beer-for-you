46ced7a7b484600193181fdb3362f636e77195c2
Daniel Roach <dannycr09@hotmail.com>
2018-03-05 17:48:18 -0500

    Added session start

--- views/inbox.php
+++ views/inbox.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once ("../model/DB.class.php");
 
 $db = new DB();
@@ -10,30 +11,33 @@ $messages = $db->getMessagesForUser($SESSION['accountID']);
 <title> WB4U | Daniels Inbox </title>
 <?php include 'inc/nav.php'?>
 <div class="container-fluid">
-	<table class="bordered centered">
-		<thead>
-			<tr>
-				<th>Title</th>
-				<th>From</th>
-				<th>Message</th>
-				<th>Date</th>
-			</tr>
-		</thead>
-		<tbody>
-			// <?php
-			//	foreach ($messages as $message) {
-			//		$from = $db->getUserByID($message->getSenderID());
-			//		$from[0]->getWholeName();
-		  //			echo "
-			//			<tr>
-			//				<td>$message->getTitle()</td>
-			//				<td>$from[0]->getWholeName()></td>
-			//				<td>$message->getContent()</td>
-			//			</tr>
-			//		";
-			//	}
-			// ?>
-		</tbody>
-	</table>
-
+	<div class="row">
+		<div class="col s10">
+			<table class="bordered centered">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>From</th>
+						<th>Message</th>
+						<th>Date</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						foreach ($messages as $message) {
+							$from = $db->getUserByID($message->getSenderID());
+							$from[0]->getWholeName();
+								echo "
+								<tr>
+									<td>$message->getTitle()</td>
+									<td>$from[0]->getWholeName()></td>
+									<td>$message->getContent()</td>
+								</tr>
+							";
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
