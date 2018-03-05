84c0c55b5d9aa57899230a5a3875b1178447d1a4
Anthony Perez <ajp8707@g.rit.edu>
2018-03-05 08:49:03 -0500

    checking login function

--- model/DB.class.php
+++ model/DB.class.php
@@ -38,7 +38,7 @@ class DB{
 		$userExists = $this->getUserByEmail($_email);
 
 		if($userExists){
-			if(password_verify($_pass, )){
+			if(password_verify($_pass, $userExists->getPassword())){
 				// $_SESSION['loggedIn'] = true;
 				// $_SESSION['accountFirstName'] = $user->getFirstName(); 
 				// $_SESSION['accountLastName'] = $user->getLastName(); 
