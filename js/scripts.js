17e523246d96cdd1eb9fdaa5994cc0c7a953cc69
Jack Corrigan <jcorrigan14@gmail.com>
2018-03-07 11:05:48 -0500

    working on ajax of styles dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,8 @@ $( document ).ready(function(){
 			});
     	});
 
-    	$('#selectedCategory').change(function(){
+    	$('#selectedCategory').change(function(e){
+				e.preventDefault();
 	        var $cat_id = $(this).val();
 					alert($cat_id);
 
