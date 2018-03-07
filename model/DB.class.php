3906ec1be834a130ee3d8bd972ae56ca4cde1d98
Jack Corrigan <jcorrigan14@gmail.com>
2018-03-07 12:34:07 -0500

    working on ajax of styles dropdown

--- model/DB.class.php
+++ model/DB.class.php
@@ -13,8 +13,8 @@ if(isset($_POST['action'])){
 			break;
 		case 'getStylesByCategory':
 			$test = 'test';
-			$cat_id = isset($_POST['category']) ? $_POST['category'] : null;
-			$value = $db->getStylesByCategory($cat_id);
+			//$cat_id = isset($_POST['category']) ? $_POST['category'] : null;
+			//$value = $db->getStylesByCategory($cat_id);
 			//return $value;
 			return $test;
 			break;
