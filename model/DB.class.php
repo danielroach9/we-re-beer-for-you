a04090885621e6a72f611d0238b6bac0b45252b0
Jack Corrigan <jcorrigan14@gmail.com>
2018-03-08 01:14:51 -0500

    working on getPreferredBeer

--- model/DB.class.php
+++ model/DB.class.php
@@ -431,7 +431,7 @@ class DB{
 																	FROM beers
 																	WHERE cat_id = ".$_category.
 																	" AND style_id = ".$_style.
-																	" AND country = ".$_country);
+																	" AND country = ".$_country."");
 			$stmt->execute();
 
 			$data = $stmt->fetchAll(PDO::FETCH_ASSOC); // does this need to change as per above when selecting multiple
