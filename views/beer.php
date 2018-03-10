874bef7b0b08a4762834dc3cae3f7e4f08d73f8a
Anthony Perez <ajp8707@g.rit.edu>
2018-03-10 14:59:20 -0500

    beer

--- views/beer.php
+++ views/beer.php
@@ -75,9 +75,8 @@ var_dump($ratings);
                     <i class='".($review->getRating() >= 2 ? "fa fa-star" : "fa fa-star-o")."'></i>
                     <i class='".($review->getRating() >= 3 ? "fa fa-star" : "fa fa-star-o")."'></i>
                     <i class='".($review->getRating() >= 4 ? "fa fa-star" : "fa fa-star-o")."'></i>
-                    <i class='".($review->getRating() >= 5 ? "fa fa-star" : "fa fa-star-o")."'></i>"
+                    <i class='".($review->getRating() >= 5 ? "fa fa-star" : "fa fa-star-o")."'></i></span>"
                     .$review->getRating()."
-                  </span>
                 </div>
                 <span class='datetime-and-location'>".$review->getLocation()."</span>
                 <span class='comment'>
