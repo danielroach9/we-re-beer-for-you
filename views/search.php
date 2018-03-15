628e6c65eb1591ca7b304d077ec6b54e2fef2332
Anthony Perez <ajp8707@g.rit.edu>
2018-03-14 20:35:20 -0400

    search test

--- views/search.php
+++ views/search.php
@@ -25,7 +25,7 @@ $styles = $db->getStylesByCategory(0); //hardcoded parameter to start
 </head>
 <body>
 	<form>
-		<input type="text" name="q" placeholder="Search for a Beer" value="<?php if(isset($_GET['q'])){ echo "{$qry}"; } ?>">
+		<input type="text" id="searchQuery" name="q" placeholder="Search for a Beer" value="<?php if(isset($_GET['q'])){ echo "{$qry}"; } ?>">
 		<a href="#" id="searchFilters">Apply Filters</a>
 		<div id="filters">
 			<div class="row">
