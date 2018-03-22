dcc39c8251e4b1d095b17885cc667cd20fc7fd51
Daniel Roach <dannycr09@hotmail.com>
2018-03-22 11:25:43 -0400

    Trying tabs instead of pagination

--- views/breweries.php
+++ views/breweries.php
@@ -3,42 +3,45 @@ require_once("../model/DB.class.php");
 
 $db = new DB();
 
-
+$breweries = $db->getAllBreweries();
+var_dump($breweries);
 ?>
 
 <?php include 'inc/header.php'?>
 <title>WB4U | Breweries</title>
 <?php include 'inc/nav.php'?>
 <div class="container-fluid">
-  <div class="row">
-    <ul class="card-panel pagination center-align">
+  <div class="row card-panel">
+    <ul class="tabs center-align">
       <!-- No breweries with first letter X-->
-      <li class="active"><a href="#!">A</a></li>
-      <li class="waves-effect"><a href="#!">B</a></li>
-      <li class="waves-effect"><a href="#!">C</a></li>
-      <li class="waves-effect"><a href="#!">D</a></li>
-      <li class="waves-effect"><a href="#!">E</a></li>
-      <li class="waves-effect"><a href="#!">F</a></li>
-      <li class="waves-effect"><a href="#!">G</a></li>
-      <li class="waves-effect"><a href="#!">H</a></li>
-      <li class="waves-effect"><a href="#!">I</a></li>
-      <li class="waves-effect"><a href="#!">J</a></li>
-      <li class="waves-effect"><a href="#!">K</a></li>
-      <li class="waves-effect"><a href="#!">L</a></li>
-      <li class="waves-effect"><a href="#!">M</a></li>
-      <li class="waves-effect"><a href="#!">N</a></li>
-      <li class="waves-effect"><a href="#!">O</a></li>
-      <li class="waves-effect"><a href="#!">P</a></li>
-      <li class="waves-effect"><a href="#!">Q</a></li>
-      <li class="waves-effect"><a href="#!">R</a></li>
-      <li class="waves-effect"><a href="#!">S</a></li>
-      <li class="waves-effect"><a href="#!">T</a></li>
-      <li class="waves-effect"><a href="#!">U</a></li>
-      <li class="waves-effect"><a href="#!">V</a></li>
-      <li class="waves-effect"><a href="#!">W</a></li>
-      <li class="waves-effect"><a href="#!">Y</a></li>
-      <li class="waves-effect"><a href="#!">Z</a></li>
-      <li class="waves-effect"><a href="#!">0-9</a></li>
+      <li class="tab"><a class="active" href="#!">A</a></li>
+      <li class="tab "><a href="#!">B</a></li>
+      <li class="tab"><a href="#!">C</a></li>
+      <li class="tab"><a href="#!">D</a></li>
+      <li class="tab"><a href="#!">E</a></li>
+      <li class="tab"><a href="#!">F</a></li>
+      <li class="tab"><a href="#!">G</a></li>
+      <li class="tab"><a href="#!">H</a></li>
+      <li class="tab"><a href="#!">I</a></li>
+      <li class="tab"><a href="#!">J</a></li>
+      <li class="tab"><a href="#!">K</a></li>
+      <li class="tab"><a href="#!">L</a></li>
+      <li class="tab"><a href="#!">M</a></li>
+      <li class="tab"><a href="#!">N</a></li>
+      <li class="tab"><a href="#!">O</a></li>
+      <li class="tab"><a href="#!">P</a></li>
+      <li class="tab"><a href="#!">Q</a></li>
+      <li class="tab"><a href="#!">R</a></li>
+      <li class="tab"><a href="#!">S</a></li>
+      <li class="tab"><a href="#!">T</a></li>
+      <li class="tab"><a href="#!">U</a></li>
+      <li class="tab"><a href="#!">V</a></li>
+      <li class="tab"><a href="#!">W</a></li>
+      <li class="tab"><a href="#!">Y</a></li>
+      <li class="tab"><a href="#!">Z</a></li>
+      <li class="tab"><a href="#!">0-9</a></li>
     </ul>
+    <div class="collection">
+    </div>
   </div>
 </div>
