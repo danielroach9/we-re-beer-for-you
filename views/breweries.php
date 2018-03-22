80b45364406fda264d442e1b988b6105c26651a4
Daniel Roach <dannycr09@hotmail.com>
2018-03-22 11:15:48 -0400

    Added all necessary letters, reformatted pagination

--- views/breweries.php
+++ views/breweries.php
@@ -11,7 +11,7 @@ $db = new DB();
 <?php include 'inc/nav.php'?>
 <div class="container-fluid">
   <div class="row">
-    <ul class="pagination center-align">
+    <ul class="card-panel pagination center-align">
       <!-- No breweries with first letter X-->
       <li class="active"><a href="#!">A</a></li>
       <li class="waves-effect"><a href="#!">B</a></li>
@@ -24,7 +24,21 @@ $db = new DB();
       <li class="waves-effect"><a href="#!">I</a></li>
       <li class="waves-effect"><a href="#!">J</a></li>
       <li class="waves-effect"><a href="#!">K</a></li>
-      <li></li>
+      <li class="waves-effect"><a href="#!">L</a></li>
+      <li class="waves-effect"><a href="#!">M</a></li>
+      <li class="waves-effect"><a href="#!">N</a></li>
+      <li class="waves-effect"><a href="#!">O</a></li>
+      <li class="waves-effect"><a href="#!">P</a></li>
+      <li class="waves-effect"><a href="#!">Q</a></li>
+      <li class="waves-effect"><a href="#!">R</a></li>
+      <li class="waves-effect"><a href="#!">S</a></li>
+      <li class="waves-effect"><a href="#!">T</a></li>
+      <li class="waves-effect"><a href="#!">U</a></li>
+      <li class="waves-effect"><a href="#!">V</a></li>
+      <li class="waves-effect"><a href="#!">W</a></li>
+      <li class="waves-effect"><a href="#!">Y</a></li>
+      <li class="waves-effect"><a href="#!">Z</a></li>
+      <li class="waves-effect"><a href="#!">0-9</a></li>
     </ul>
   </div>
 </div>
