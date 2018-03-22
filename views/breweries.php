cb2ba9ffd0ceb8c01b8b47badb7c19aea23c2821
Daniel Roach <dannycr09@hotmail.com>
2018-03-22 11:11:02 -0400

    Creating pagination for breweries.php

--- views/breweries.php
+++ views/breweries.php
@@ -10,5 +10,21 @@ $db = new DB();
 <title>WB4U | Breweries</title>
 <?php include 'inc/nav.php'?>
 <div class="container-fluid">
-
+  <div class="row">
+    <ul class="pagination center-align">
+      <!-- No breweries with first letter X-->
+      <li class="active"><a href="#!">A</a></li>
+      <li class="waves-effect"><a href="#!">B</a></li>
+      <li class="waves-effect"><a href="#!">C</a></li>
+      <li class="waves-effect"><a href="#!">D</a></li>
+      <li class="waves-effect"><a href="#!">E</a></li>
+      <li class="waves-effect"><a href="#!">F</a></li>
+      <li class="waves-effect"><a href="#!">G</a></li>
+      <li class="waves-effect"><a href="#!">H</a></li>
+      <li class="waves-effect"><a href="#!">I</a></li>
+      <li class="waves-effect"><a href="#!">J</a></li>
+      <li class="waves-effect"><a href="#!">K</a></li>
+      <li></li>
+    </ul>
+  </div>
 </div>
