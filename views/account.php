a072201fdec7242abf0711751e63d41cd8195319
Moises Lora Perez (RIT Student) <mal3941@ist-serenity.main.ad.rit.edu>
2018-03-26 02:43:36 -0400

    Fixed register and added account styling 2

--- views/account.php
+++ views/account.php
@@ -1,11 +1,9 @@
 <?php include "inc/header.php";?>
 <title>We're beer for you!</title>
 <?php include "inc/nav.php"; ?>
-	<form>
-		<label>First Name</label>
-		<input type="text" name="f_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
+<!-- 	<form>
+		<label></label>
 		<label>Last Name</label>
-		<input type="text" name="l_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
 		<label>Email</label>
 		<input type="text" name="email" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
 		<label>Password</label>
@@ -15,5 +13,43 @@
 	</form>
 	<label>Preference</label>
 		<button>Update Preferences</button>
-
-<?php include "inc/footer.php"; ?>
+ -->
+<div class="section no-pad-bot">
+    <div class="container">
+      <div class="valign-wrapper row login-box">
+        <div class="card hoverable col s6 offset-s3">
+          <form id="loginForm">
+            <div class="card-content">
+              <span class="card-title center-align"><h4>Log in</h4></span>
+              <div class="row">
+                <div class="input-field col s12">
+                  <label for="f_name">First Name</label>
+				  <input type="text" name="f_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
+                </div>
+                <div class="input-field col s12">
+				<label for="l_name">Last Name</label>
+				<input type="text" name="l_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
+                </div>
+				<div class="input-field col s12">
+				<label for="email">Email</label>
+				<input type="text" name="email" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
+                </div>
+				<div class="input-field col s12">
+				<label for="pass">Password</label>
+				<input type="password" name="pass">
+                </div>
+              </div>
+            </form>
+			<div class="row center-align">
+                <button class="btn brown waves-effect waves-light">Update Info</button>
+              </div>
+			<div class="row card-action center-align">
+			  	<h5> Preferences </h5>
+                <button class="btn brown waves-effect waves-light">Update Preferences</button>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include "inc/footer.php"; ?>
