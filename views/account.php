3c52b6a3d1885a679fb09bc044c7ffadd9e45b81
Anthony Perez <ajp8707@g.rit.edu>
2018-03-25 20:23:13 -0400

    js

--- views/account.php
+++ views/account.php
@@ -1,15 +1,18 @@
-<?php include "inc/header.php"; ?>
+<?php include "inc/header.php"; 
+	$db = new DB();
+	$user = $db->getUserByID($_SESSION['accountID']);
+?>
 <title>We're beer for you!</title>
 <?php include "inc/nav.php"; ?>
 	<form>
 		<label>First Name</label>
 		<input type="text" name="f_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
 		<label>Last Name</label>
-		<input type="text" name="l_name">
+		<input type="text" name="l_name" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
 		<label>Email</label>
-		<input type="text" name="email">
+		<input type="text" name="email" placeholder="<?php echo $_SESSION['accountFirstName'];?>">
 		<label>Password</label>
-		<input type="password" name="pass">
+		<input type="text" name="pass" placeholder="<?php echo $user['pass'];?>">
 
 		<button>Update Info</button>
 	</form>
